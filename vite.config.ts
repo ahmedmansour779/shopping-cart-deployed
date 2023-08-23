2df8d4a718d0d981bad1f598c3f89f3e5a500696
ahmedmansour779 <ahmedmansour1239@gmail.com>
2023-08-23 13:55:25 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shopping-cart/"
+  base: "/shopping-cart-deployed/"
 })
